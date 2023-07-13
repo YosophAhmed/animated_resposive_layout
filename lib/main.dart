b899d77082540bd7ebfa2366878fa6f5f9c4bcd8
yosoph <devyosoph@gmail.com>
2023-07-13 11:37:47 +0300

    Animating the transitions

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:animated_responsive_layout/widgets/animated_floating_action_button.dart';
+import 'package:animated_responsive_layout/widgets/disappearing_bottom_navigation_bar.dart';
+import 'package:animated_responsive_layout/widgets/disappearing_navigation_rail.dart';
 import 'package:flutter/material.dart';
 
-import 'destinations/destinations.dart';
+import 'animations.dart';
 import 'models/data.dart' as data;
 import 'models/models.dart';
 import 'widgets/email_list_view.dart';
@@ -34,50 +37,105 @@ class Feed extends StatefulWidget {
   State<Feed> createState() => _FeedState();
 }
 
-class _FeedState extends State<Feed> {
+class _FeedState extends State<Feed> with SingleTickerProviderStateMixin {
   late final _colorScheme = Theme.of(context).colorScheme;
   late final _backgroundColor = Color.alphaBlend(
       _colorScheme.primary.withOpacity(0.14), _colorScheme.surface);
 
+  late final _controller = AnimationController(
+    duration: const Duration(milliseconds: 1000),
+    reverseDuration: const Duration(milliseconds: 1250),
+    value: 0,
+    vsync: this,
+  );
+  late final _railAnimation = RailAnimation(parent: _controller);
+  late final _railFabAnimation = RailFabAnimation(parent: _controller);
+  late final _barAnimation = BarAnimation(parent: _controller);
+
   int selectedIndex = 0;
+  bool controllerInitialized = false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    final double width = MediaQuery.of(context).size.width;
+
+    final AnimationStatus status = _controller.status;
+    if (width > 600) {
+      if (status != AnimationStatus.forward &&
+          status != AnimationStatus.completed) {
+        _controller.forward();
+      }
+    } else {
+      if (status != AnimationStatus.reverse &&
+          status != AnimationStatus.dismissed) {
+        _controller.reverse();
+      }
+    }
+    if (!controllerInitialized) {
+      controllerInitialized = true;
+      _controller.value = width > 600 ? 1 : 0;
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: _backgroundColor,
-        child: EmailListView(
-          currentUser: widget.currentUser,
-          selectedIndex: selectedIndex,
-          onSelected: (index) {
-            setState(() {
-              selectedIndex = index;
-            });
-          },
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: _colorScheme.tertiaryContainer,
-        foregroundColor: _colorScheme.onTertiaryContainer,
-        onPressed: () {},
-        child: const Icon(Icons.add),
-      ),
-      bottomNavigationBar: NavigationBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        destinations: destinations.map<NavigationDestination>((d) {
-          return NavigationDestination(
-            icon: Icon(d.icon),
-            label: d.label,
-          );
-        }).toList(),
-        selectedIndex: selectedIndex,
-        onDestinationSelected: (index) {
-          setState(() {
-            selectedIndex = index;
-          });
-        },
-      ),
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, _) {
+        return Scaffold(
+          body: Row(
+            children: [
+              DisappearingNavigationRail(
+                railAnimation: _railAnimation,
+                railFabAnimation: _railFabAnimation,
+                selectedIndex: selectedIndex,
+                backgroundColor: _backgroundColor,
+                onDestinationSelected: (index) {
+                  setState(() {
+                    selectedIndex = index;
+                  });
+                },
+              ),
+              Expanded(
+                child: Container(
+                  color: _backgroundColor,
+                  child: EmailListView(
+                    selectedIndex: selectedIndex,
+                    onSelected: (index) {
+                      setState(() {
+                        selectedIndex = index;
+                      });
+                    },
+                    currentUser: widget.currentUser,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          floatingActionButton: AnimatedFloatingActionButton(
+            animation: _barAnimation,
+            onPressed: () {},
+            child: const Icon(Icons.add),
+          ),
+          bottomNavigationBar: DisappearingBottomNavigationBar(
+            barAnimation: _barAnimation,
+            selectedIndex: selectedIndex,
+            onDestinationSelected: (index) {
+              setState(() {
+                selectedIndex = index;
+              });
+            },
+          ),
+        );
+      },
     );
   }
 }
